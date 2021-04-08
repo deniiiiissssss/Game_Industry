1e1d0713f20bc0da90d827f75263425a8a80d6cd
deniiiiissssss <veretelnickov.d@ya.ru>
2021-04-08 23:02:11 +0300

    Live update games volume by two dropdown

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pandas as pd
 import plotly.express as px
 
-#df = pd.read_csv('games.csv')
-#fig = px.area(df, x="year", y="pop", color="continent",line_group="country")
-
+#MAIN DATAFRAME
 games_df = pd.read_csv('games.csv')
-games_df = games_df.loc[(games_df['Year_of_Release']>=2000)].dropna()
+games_df = games_df.loc[(games_df['Year_of_Release']>=2000)].dropna()\
+    .rename(columns={'User_Score':'User Score','Critic_Score':'Critic Score'})
 
-platform_year_df = pd.DataFrame(games_df.groupby('Platform')['Year_of_Release'].value_counts()).rename(columns={'Year_of_Release':'Counts'}).reset_index()
+platform_year_df = games_df.groupby(["Platform", "Year_of_Release"]).size().reset_index(name="Volume")
 
-fig = px.area(platform_year_df, x="Year_of_Release", y="Counts", color="Platform",line_group="Platform")
+platform_year_fig = px.area(platform_year_df, x='Year_of_Release', y='Volume', color='Platform',\
+              title="Games Volume by Platform and Year")
 
 
+scores_genres_fig = px.scatter(games_df, x="User Score", y="Critic Score", color="Genre",\
+                               title="Scores by Gengers")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -44,66 +48,81 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
     html.Div(style={'width':'100%','height':'0.5px','background-color':'#e5e5e5','margin-bottom':'20px'}),
 
-    html.Div(style={'width':'20%','display':'flex','flex-direction':'row'},children=[
+    html.Div(style={'width':'50%','display':'flex','flex-direction':'row'},children=[
 
         html.Label('Genre:',style={'padding':'5px'}),
-        dcc.Dropdown(style={'width':'250px'}, #genre multiple choise list
+        dcc.Dropdown(style={'width':'100%'}, #genre multiple choise list
             id='genre_dropdown',
             options=[
-                {'label': 'New York City', 'value': 'NYC'},
+                {'label': i, 'value': i} for i in games_df.Genre.unique()
                     #genres from dataframe
                 ],
             placeholder='Select game genres',
-            multi=True
+            multi=True,
+            searchable=True,
         ),
+        html.Div(id='genre_selected'),
 
         html.Label('Rating:',style={'padding':'5px'}),
-        dcc.Dropdown(style={'width':'250px'}, #rating multiple choise list
+        dcc.Dropdown(style={'width':'100%'}, #rating multiple choise list
             id='rating_dropdown',
             options=[
-                {'label': 'New York City', 'value': 'NYC'},
-                    #ratings from dataframe
-                ],
+                {'label': i, 'value': i} for i in games_df.Rating.unique()
+            ],
             placeholder='Select game ratings',
-            multi=True
-        )
+            multi=True,
+            searchable=True,
+        ),
+        html.Div(id='rating_selected'),
+
     ]),
 
     html.Br(),
 
     html.Div(style={'width':'10%','display':'flex','flex-direction':'row'},children=[
-        html.Label('Game Volume:',style={'margin':'0.5em',}),
-        html.B('25',style={'padding':'5px','font-size':'14pt'}), #game volume text
+        html.Label('Games Volume:',style={'margin':'0.5em',}),
+        html.Div(id='games_volume_selected',style={'padding':'5px','font-size':'14pt'}), #game volume text
     ]),
 
     html.Div(style={'display':'flex','flex-direction':'row'},children=[
-    dcc.Graph(figure=fig, style={'width':'50%'}, #graph
+    dcc.Graph(figure=platform_year_fig, style={'width':'50%'}, #graph
         id='game_graph',
     ),
-    dcc.Graph(style={'width':'50%'}, #graph
-            id='game_graph_2',
-            figure={ #Scatter plot Scores by Gengers
-                'data': [
-                    {'x': [1,2], 'y': [1,2,3,4], 'type': 'line', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'plot_bgcolor': colors['background_graph'],
-                    'paper_bgcolor': colors['background'],
-                    'title': 'Scores by Genres',
-                }
-            }
+
+    dcc.Graph(figure=scores_genres_fig,style={'width':'50%'}, #graph
+            id='scores_graph',
         )
     ]),
 
-    html.Label('Release Dates:',style={'padding':'5px',}),
+    html.Label('Release Dates:',style={'padding':'5px'}),
     dcc.DatePickerRange(
-        start_date_placeholder_text="Start Date",
-        end_date_placeholder_text="End Date",
+        id="gade_range",
+        start_date_placeholder_text='Start Date',
+        end_date_placeholder_text='End Date',
         calendar_orientation='horizontal',
         with_portal=True,
     ),
 ])
+
+@app.callback(
+    Output(component_id='games_volume_selected', component_property='children'),
+    Input(component_id='genre_dropdown', component_property='value'),
+    Input(component_id='rating_dropdown', component_property='value')
+)
+def update_output_div(genre_dropdown_value,rating_dropdown_value):
+    return '{}'.format(len(games_df.loc[(games_df['Genre'].isin(list(genre_dropdown_value))) |\
+                                        (games_df['Rating'].isin(list(rating_dropdown_value)))]))
+
+# @app.callback(
+#     Output(component_id='rating_selected', component_property='children'),
+#     Input(component_id='rating_dropdown', component_property='value')
+# )
+# def update_output_div(input_value):
+#     return html.H4(input_value)
+#     print(input_value)
+
+
+
 
 
 if __name__ == '__main__':
