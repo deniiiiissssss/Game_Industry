36d3eabe1bc17c0dd21fa31492c4a1aeaba7e2bf
deniiiiissssss <veretelnickov.d@ya.ru>
2021-04-08 16:29:04 +0300

    Edit markup: genres/scores multiple choice fields - horizontal alignment graphics - horizontal alignment added simple line after desc some simple alignments

--- app.py
+++ app.py
@@ -25,46 +25,55 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         style={
             'textAlign': 'left',
             'color': colors['text'],
-            'padding':'0.2em',
+            'margin-bottom':'10px',
         }
     ),
 
-    html.P('Explore the gaming industry by metrics such as: Genres, Ratings, Games by Platform and Year, Dependence of Genres on Player and Critics Reviews. Just choose the genres and ratings you are interested in.',
+    html.P('Explore the gaming industry by metrics such as: Genres, Ratings, Games by Platform and Year, Dependence of Genres on Player and Critics Reviews. Just select the genres and ratings you are interested in.',
         style={ #Dashboard Desc
             'margin':'0.5em',
             'width':'50%',
         }
     ),
-        dcc.Dropdown( #genre multiple choise list
-                    id='genre_dropdown',
-                    options=[
-                        {'label': 'New York City', 'value': 'NYC'},
-                        {'label': 'Montreal', 'value': 'MTL'},
-                        {'label': 'San Francisco', 'value': 'SF'}
-                    ],
-                    value='Choose game genres',
-                multi=True
-                ),
-        dcc.Dropdown( #rating multiple choise list
-                    id='rating_dropdown',
-                    options=[
-                        {'label': 'New York City', 'value': 'NYC'},
-                        {'label': 'Montreal', 'value': 'MTL'},
-                        {'label': 'San Francisco', 'value': 'SF'}
-                    ],
-                    value='Choose game ratings',
-                multi=True
-                ),
-    html.Br(),
-    html.P('Game Volume:',
-            style={
-                'margin':'0.5em',
-                'width':'50%',
-            }
-        ),
-    html.H4('25',style={'margin':'0.5em'}), #game volume text
 
-    dcc.Graph( #graph
+    html.Div(style={'width':'100%','height':'0.5px','background-color':'#e5e5e5','margin-bottom':'20px'}),
+    html.Div(style={'width':'20%','display':'flex','flex-direction':'row'},children=[
+        html.Label('Genre:',style={'padding':'5px'}),
+        dcc.Dropdown(style={'width':'250px'}, #genre multiple choise list
+            id='genre_dropdown',
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                    #genres from dataframe
+                ],
+            placeholder='Select game genres',
+            multi=True
+        ),
+
+        html.Label('Rating:',style={'padding':'5px'}),
+        dcc.Dropdown(style={'width':'250px'}, #rating multiple choise list
+            id='rating_dropdown',
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                    #ratings from dataframe
+                ],
+            placeholder='Select game ratings',
+            multi=True
+        )
+    ]),
+
+    html.Br(),
+    html.Div(style={'display':'flex','flex-direction':'column'},children=[
+    html.Label('Game Volume:',
+                style={
+                    'margin':'0.5em',
+                    'width':'50%',
+                }
+            ),
+    html.B('25',style={'padding':'5px','font-size':'16pt'}), #game volume text
+    ]),
+
+    html.Div(style={'display':'flex','flex-direction':'row'},children=[
+    dcc.Graph(style={'width':'50%'}, #graph
         id='game_graph',
         figure={
             'data': [
@@ -74,13 +83,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             'layout': {
                 'plot_bgcolor': colors['background_graph'],
                 'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
+                'title':'Games by Year and Platforms',
             }
         }
     ),
-    dcc.Graph( #graph
+    dcc.Graph(style={'width':'50%'}, #graph
             id='game_graph_2',
             figure={
                 'data': [
@@ -90,9 +97,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                 'layout': {
                     'plot_bgcolor': colors['background_graph'],
                     'paper_bgcolor': colors['background'],
+                    'title': 'Scores by Genres',
                 }
             }
-        ),
+        )
+    ]),
     dcc.DatePickerRange(
         id="date-picker-select",
         start_date=dt(2014, 1, 1),
